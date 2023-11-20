76c953b63d7022d8757f9654b82a4fea30a8fbdf
Glasiem <maksym.tyulpa@gmail.com>
2023-11-20 20:44:30 +0200

    Initial commit

---
+++ src/main/java/org/example/component/column/ColumnType.java
@@ -0,0 +1,12 @@
+package org.dbms.database.component.column;
+
+public enum ColumnType {
+    INT,
+    CHAR,
+    REAL,
+    STRING,
+
+    MONEY,
+    MONEY_INVL
+
+}
---
+++ src/main/java/org/example/remote/RemoteDB.java
@@ -0,0 +1,2 @@
+package org.example.remote;public class RemoteDB {
+}
